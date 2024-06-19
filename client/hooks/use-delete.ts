51ee4d7647bb500d7710c8179ddf54f09210960d
Lei Zheng <zhenglei530@gmail.com>
2024-06-20 10:52:52 +1200

    added queryKey in invalidateQueries

--- client/hooks/use-delete.ts
+++ client/hooks/use-delete.ts
@@ -1,16 +1,18 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import request from 'superagent'
+const API_HOST = 'https://paataka.cloud/api/_/team-todo'
+const TOKEN = 'inboT4cuIWA'
 
 export function useDelete(id: number) {
   const queryClient = useQueryClient()
   return useMutation({
     mutationFn: async (data) => {
       await request
-        .delete(`https://paataka.cloud/api/_/team-todo/todo/${id}`)
-        .auth('inboT4cuIWA', { type: 'bearer' })
+        .delete(`${API_HOST}/todo/${id}`)
+        .auth(TOKEN, { type: 'bearer' })
     },
     onSuccess: () => {
-      queryClient.invalidateQueries(['todos', id])
+      queryClient.invalidateQueries({ queryKey: ['todos', id] })
     },
   })
 }
