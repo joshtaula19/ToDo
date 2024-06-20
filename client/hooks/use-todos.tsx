0624e7482c0e81cee018c6715b24868d6ee0e3e0
Chris Hofsteede <chris@hofsteede.com>
2024-06-20 10:14:21 +1000

    create useCreateTodo()

--- client/hooks/use-todos.tsx
+++ client/hooks/use-todos.tsx
@@ -2,7 +2,7 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import request from 'superagent'
 // import { API_HOST, TOKEN } from '../../.env'
 
-const API_HOST = 'https://paataka.cloud/api/_/team-todo/'
+const API_HOST = 'https://paataka.cloud/api/_/team-todo'
 const TOKEN = 'inboT4cuIWA'
 
 export function useToDos() {
@@ -10,12 +10,31 @@ export function useToDos() {
     queryKey: ['todos'],
     queryFn: async () => {
       const res = await request
-        .get(`${API_HOST}todo`)
+        .get(`${API_HOST}/todo`)
         .auth(TOKEN, { type: 'bearer' })
 
       // console.log('api call:', res.body)
 
       return res.body
+    },
+  })
+}
+
+export function useCreateToDo() {
+  const qc = useQueryClient()
+
+  return useMutation({
+    mutationFn: async (text) => {
+      const res = await request
+        .post(`${API_HOST}/todo`)
+        .auth(TOKEN, { type: 'bearer' })
+        .send({ text })
+
+      return res.body
+    },
+
+    onSuccess: async () => {
+      qc.invalidateQueries({ queryKey: ['todos'] })
     },
   })
 }
